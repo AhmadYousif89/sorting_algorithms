c21c253d8444ae809a98e6dc62369e556799cf9e
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-16 23:51:51 +0200

    added check for null array

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -21,15 +21,18 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, new_size;
 
+	if (array == NULL)
+		return;
+
 	while (size > 0)
 	{
 		new_size = 0;
-		for (i = 1; i < size; i++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[i - 1] > array[i])
+			if (array[i] > array[i + 1])
 			{
-				swap(&array[i - 1], &array[i]);
-				new_size = i;
+				swap(&array[i], &array[i + 1]);
+				new_size = i + 1;
 				print_array(array, size);
 			}
 		}
