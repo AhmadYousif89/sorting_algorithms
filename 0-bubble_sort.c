688fedd50326575846b40e6b47c23bf93201e85b
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-17 00:11:06 +0200

    .

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,44 @@
 #include "sort.h"
 
 /**
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
+ */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
  * bubble_sort - The bubble sorting algorithm
  * @array: pointer to the array
  * @size: size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-	int temp;
-	size_t i, n;
+	size_t i, len = size;
+	int bubbly = 0;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (n = size; n > 0; n--)		   /* O(n) */
-		for (i = 0; i < size - 1; i++) /* O(n) */
+	while (bubbly == 0)
+	{
+		bubbly = 1;
+		for (i = 0; i < len - 1; i++)
+		{
 			if (array[i] > array[i + 1])
 			{
-				temp = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = temp;
+				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
+				bubbly = 0;
 			}
+		}
+		len--;
+	}
 } /* O(n^2) */
