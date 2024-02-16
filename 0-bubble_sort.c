9dc53fac77d4b46584304463d746fed3109658bc
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-16 23:59:23 +0200

    replace the swap function with in-place swap

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,38 +1,25 @@
 #include "sort.h"
 
 /**
- * swap - Swap two numbers
- * @a: pointer the 1st number
- * @b: pointer the 2nd number
- */
-void swap(int *a, int *b)
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
-
-/**
  * bubble_sort - The bubble sorting algorithm
  * @array: pointer to the array
  * @size: size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
+	int temp;
 	size_t i, n;
 
 	if (array == NULL)
 		return;
 
-	for (n = size; n > 0; n--)
-	{
-		for (i = 0; i < size - 1; i++)
-		{
+	for (n = size; n > 0; n--)		   /* O(n) */
+		for (i = 0; i < size - 1; i++) /* O(n) */
 			if (array[i] > array[i + 1])
 			{
-				swap(&array[i], &array[i + 1]);
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
 				print_array(array, size);
 			}
-		}
-	}
-}
+} /* O(n^2) */
