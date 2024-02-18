2f86c00a8159f6013f1579267ab1c3504dca9b1c
AhmadYousif89 <ayob.4wrk89@gmail.com>
2024-02-18 21:14:37 +0200

    code_refactor: swap values using arithmatic swap

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -2,15 +2,14 @@
 
 /**
  * selection_sort - The selection sorting algorithm
- * @array: pointer to int
+ * @arr: pointer to the array
  * @size: size of the array
  */
-void selection_sort(int *array, size_t size)
+void selection_sort(int *arr, size_t size)
 {
 	size_t i, j_min, j;
-	int tmp;
 
-	if (array == NULL || size < 2)
+	if (arr == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
@@ -19,15 +18,15 @@ void selection_sort(int *array, size_t size)
 		j_min = i;
 		/* update the index of j_min if it has greater value than index j */
 		for (j = i + 1; j < size; j++)
-			if (array[j] < array[j_min])
+			if (arr[j] < arr[j_min])
 				j_min = j;
 
-		if (array[i] > array[j_min])
-		{
-			tmp = array[i];
-			array[i] = array[j_min];
-			array[j_min] = tmp;
-			print_array(array, size);
+		if (arr[i] > arr[j_min])
+		{ /* arithmatic swap */
+			arr[i] = arr[i] + arr[j_min];
+			arr[j_min] = arr[i] - arr[j_min];
+			arr[i] = arr[i] - arr[j_min];
+			print_array(arr, size);
 		}
 	}
 }
